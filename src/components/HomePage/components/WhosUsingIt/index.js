a3c567b558a3114748b6547cb3dfc195a0c141b9
Jason <jasonbahl@mac.com>
2018-01-18 14:49:37 -0700

    no message

--- src/components/HomePage/components/WhosUsingIt/index.js
+++ src/components/HomePage/components/WhosUsingIt/index.js
@@ -3,6 +3,9 @@ import { Row, Col } from 'antd'
 import styled from 'styled-components'
 import QuartzyLogo from './img/quartzy.svg'
 import WorkQuartzLogo from './img/work.qz.svg'
+import TwinCitiesLogo from './img/twincities.png'
+import DenverPostLogo from './img/denverpost.svg'
+import HopeLabLogo from './img/hopelab.png'
 
 const Logo = styled.div`
   padding: 50px 0;
@@ -12,9 +15,16 @@ const Logo = styled.div`
   }
 `;
 
+const StlyedHeadline = styled.h3`
+  text-align: center;
+  font-size: 30px;
+  margin-top:50px;
+`;
+
 const WhosUsingIt = () => (
   <Row type="flex" justify="space-around" align="middle">
     <Col span={18}>
+      <StlyedHeadline>Who's Using WPGraphQL?</StlyedHeadline>
       <Row type="flex" justify="space-around" align="middle" gutter={16}>
         <Col xl={3} lg={4} md={8} sm={12} xs={12}>
           <Logo><img src={QuartzyLogo} /></Logo>
@@ -23,13 +33,13 @@ const WhosUsingIt = () => (
           <Logo><img src={WorkQuartzLogo} /></Logo>
         </Col>
         <Col xl={3} lg={4} md={8} sm={12} xs={12}>
-          <Logo>Denver Post</Logo>
+          <Logo><img src={TwinCitiesLogo} /></Logo>
         </Col>
         <Col xl={3} lg={4} md={8} sm={12} xs={12}>
-          <Logo>Digital First Media</Logo>
+          <Logo><img src={DenverPostLogo} /></Logo>
         </Col>
         <Col xl={3} lg={4} md={8} sm={12} xs={12}>
-          <Logo>Hope Labs</Logo>
+          <Logo><img src={HopeLabLogo} /></Logo>
         </Col>
       </Row>
     </Col>
