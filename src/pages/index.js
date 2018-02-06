c475a8a75f43db8c9c52a8151e1cc228778c9a80
Jason <jasonbahl@mac.com>
2018-02-06 13:36:50 -0700

    no message

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ const IndexPage = () => (
         <Title>GraphQL API for WordPress</Title>
         <SubTitle>WPGraphQL is a free, open-source WordPress plugin that provides an extendable GraphQL schema and API for any WordPress site.</SubTitle>
         <Button type="primary" size="large">
-          <Link to="/getting-started/about" ><Icon type="file" /> Docs</Link>
+          <Link to="/docs/getting-started/about" ><Icon type="file" /> Docs</Link>
         </Button>  <Button type="primary" size="large">
           <Link to="https://github.com/wp-graphql/wp-graphql" ><Icon type="github" /> View on Github</Link>
       </Button>
